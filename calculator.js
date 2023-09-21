a7a04005b6d3afc99a8edcacd5fb87a2edf46471
Spencer Peterson <spencerpeterson.wi@gmail.com>
2023-09-21 10:28:21 -0600

    added calculate function code

--- calculator.js
+++ calculator.js
@@ -1,71 +1,95 @@
 // STUDENT FUNCTIONS
 
 function add(input1, input2) {
-  if (!sanitizeInput(input1, input2)) {
-    return "AAAAAA";
-  }
-
   return input1 + input2;
 }
 
 function subtract(input1, input2) {
-  sanitizeInput(input1, input2);
-
   return input1 - input2;
 }
 
 function multiply(input1, input2) {
-  sanitizeInput(input1, input2);
-
   return input1 * input2;
-
 }
 
 function divide(input1, input2) {
-  sanitizeInput(input1, input2);
-
   return input1 / input2;
 }
 
 function power(input1, input2) {
-  sanitizeInput(input1, input2);
-
   return input1 ** input2;
 }
 
 function modulus(input1, input2) {
-  sanitizeInput(input1, input2);
-
   return input1 % input2;
 }
 
 function sqrt(input) {
-  if (!+input) {
-    return "Invalid Input"
-  }
-  
   return Math.sqrt(input);
-
 }
 
-// Check if both inputs are numbers
-function sanitizeInput(input1, input2) {
-  inputNum1 = +input1;
-  inputNum2 = +input2;
 
-  if(!inputNum1) {
-    console.log("Input Invalid");
-    return false;
+function calculate(expression) {
+  expression = expression.trim();
+
+  // Turn the given expression into an array
+  // set variables to array values
+  const tokens = expression.split(' ');
+  let num1;
+  let operator;
+  let num2;
+  
+  // if there's two arguments, it's a sqrt operation
+  if (tokens.length === 2) {
+    operator = tokens[0];
+    num2 = +tokens[1];
+
+    console.log(num1);
+    console.log(operator);
+    console.log(num2);
+
+  }
+  else if (tokens.length === 3) { // if there are three arguments, it's a normal expression
+    num1 = +tokens[0];
+    operator = tokens[1];
+    num2 = +tokens[2];
+
+    console.log(num1);
+    console.log(operator);
+    console.log(num2);
   }
   else {
-    console.log("Input Valid");
-    return true;
+    alert("Please enter your equation in the formats shown on the page.");
+  }
+
+  if(Number.isNaN(num1) || Number.isNaN(num2)) {
+    alert("Only numbers can be entered into the calculator.");
+    alert("This calculator only takes 2 numbers and an operator at a time.");
+    alert("Make sure there is a space between each argument.");
+    return;
+  }
+
+  // do a mathematical operation depending on the operator
+  switch(operator) {
+    case ('+') :
+      return add(num1, num2);
+    case ('-') :
+      return subtract(num1, num2);
+    case ('*') :
+      return multiply(num1, num2);
+    case ('/') :
+      return divide(num1, num2);
+    case ('^') :
+      return power(num1, num2);
+    case ('%') :
+      return modulus(num1, num2);
+    case ('sqrt') :
+      return sqrt(num2);
+    default:
+      return "That operator is not supported.";
   }
 }
 
-
-
-function calculate(expression) {}
 
 /* **************** DO NOT EDIT THE CODE BELOW **************** */
 /* ************************************************************ */
@@ -79,3 +103,5 @@ document.querySelector('#submitButton').addEventListener('click', () => {
     document.querySelector('#answer').innerText = result;
   }
 });
+
+
