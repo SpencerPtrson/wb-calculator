87c05fce060246b57becf68778c6736f46031168
Spencer Peterson <spencerpeterson.wi@gmail.com>
2023-09-21 09:42:44 -0600

    Added addition, subtraction, multiplication, division, power, modulus, and sqrt

--- calculator.js
+++ calculator.js
@@ -1,3 +1,70 @@
+// STUDENT FUNCTIONS
+
+function add(input1, input2) {
+  if (!sanitizeInput(input1, input2)) {
+    return "AAAAAA";
+  }
+
+  return input1 + input2;
+}
+
+function subtract(input1, input2) {
+  sanitizeInput(input1, input2);
+
+  return input1 - input2;
+}
+
+function multiply(input1, input2) {
+  sanitizeInput(input1, input2);
+
+  return input1 * input2;
+
+}
+
+function divide(input1, input2) {
+  sanitizeInput(input1, input2);
+
+  return input1 / input2;
+}
+
+function power(input1, input2) {
+  sanitizeInput(input1, input2);
+
+  return input1 ** input2;
+}
+
+function modulus(input1, input2) {
+  sanitizeInput(input1, input2);
+
+  return input1 % input2;
+}
+
+function sqrt(input) {
+  if (!+input) {
+    return "Invalid Input"
+  }
+  
+  return Math.sqrt(input);
+
+}
+
+// Check if both inputs are numbers
+function sanitizeInput(input1, input2) {
+  inputNum1 = +input1;
+  inputNum2 = +input2;
+
+  if(!inputNum1) {
+    console.log("Input Invalid");
+    return false;
+  }
+  else {
+    console.log("Input Valid");
+    return true;
+  }
+}
+
+
+
 function calculate(expression) {}
 
 /* **************** DO NOT EDIT THE CODE BELOW **************** */
